1994b8f319f960185a89956830c28312b9c35e99
Arch-Noize <santiagonoize@gmail.com>
2023-12-08 11:06:56 -0400

    Add forms and fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-    resources :comments, only: [:new, :create]
-    resources :likes, only: [:create]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end
