c66c51f7a051a68d487c4ca8a90e68db8e1233dd
Arch-Noize <santiagonoize@gmail.com>
2023-12-08 10:24:01 -0400

    Add basic files

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 end
