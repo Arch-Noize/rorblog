28720e4f7d9a60a0fb6015614a9d03767060d051
Arch-Noize <santiagonoize@gmail.com>
2023-12-08 10:32:01 -0400

    Update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      flash[:sucess] = 'Post created successfuly!'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error, the post cannot be created!'
+      render :new, locals: { post: @post }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
