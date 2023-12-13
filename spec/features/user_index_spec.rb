92f4101d1566f0cae6074db444144b5673a18fdf
Arch-Noize <santiagonoize@gmail.com>
2023-12-13 06:35:23 -0400

    Fix errors and remove needless files

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Testing Post index view, it', type: :feature do
+RSpec.describe 'Testing User index view, it', type: :feature do
   before :each do
     @user1 = User.create(name: 'Santiago', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 2)
     @user2 = User.create(name: 'Ben', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 4)
@@ -20,13 +20,13 @@ RSpec.describe 'Testing Post index view, it', type: :feature do
     expect(page).to have_text("Mike\nNumber of posts: 8")
   end
 
+  it 'should return the correct css ' do
+    3.times { expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']") }
+  end
+
   it 'should redirect to user/show, when a user is clicked.' do
     click_on 'Santiago'
     expect(page).to have_content 'Santiago'
     expect(current_path).to eq(user_path(@user1))
-  end
-
-  it 'should return the correct css ' do
-    3.times { expect(page).to have_css("img[src*='https://unsplash.com/photos/F_-0BxGuVvo']") }
   end
 end
