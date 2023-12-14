88e4c0e4548a3b7d9362c75e53dc63448713e99f
Arch-Noize <santiagonoize@gmail.com>
2023-12-14 18:33:00 -0400

    Temporary SMTP fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,24 +38,23 @@ Rails.application.configure do
 
   # Config default url options for ActionMailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-
+  
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'localhost:3000', # your app domain
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: 'santiagonoize@gmail.com',
-    password: 'R0x4nn3_12' # your Gmail password or App Password
+    :user_name => 'f011029bdd44d0', 
+    :password => 'b70387978eb827', 
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
   }
 
   # Print deprecation notices to the Rails logger.
