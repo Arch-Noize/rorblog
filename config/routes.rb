88463f898e80a1c3abedbbaf2d15137c30018c7a
Arch-Noize <santiagonoize@gmail.com>
2023-12-15 06:19:06 -0400

    Add missing deletion function

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create, :delete] do
+      resources :comments, only: [:new, :create, :delete]
       resources :likes, only: [:create]
     end
   end
