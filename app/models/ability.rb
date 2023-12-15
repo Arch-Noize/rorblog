06c139b48bcc8e41d1a66d99fcf26c62e6492164
Arch-Noize <santiagonoize@gmail.com>
2023-12-15 06:44:18 -0400

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     user ||= User.new
-    
+
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
     can :read, :all
 
     return unless user.admin?
+
     can :manage, :all
   end
 end
