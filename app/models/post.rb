5fd3acc40c8f89535e0e7635a48863f4596eb473
Arch-Noize <santiagonoize@gmail.com>
2023-12-15 06:35:17 -0400

    Remove wrongly placed function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy, counter_cache: true
-  has_many :like
+  has_many :likes
   belongs_to :author, class_name: 'User'
 
   validates :title, presence: true, length: { maximum: 250 }
@@ -12,17 +12,6 @@ class Post < ApplicationRecord
 
   def recent_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
-  end
-
-  def destroy
-    @post = Post.includes(:likes).find(params[:id])
-    Comment.where(post_id: @post.id).destroy_all
-    @author = @post.author
-    @author.decrement!(:post_counter)
-    @post.likes.destroy_all
-    @post.destroy
-
-    redirect_to user_posts_path(id: @author.id), notice: 'Post successfully deleted'
   end
 
   private
